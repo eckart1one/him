cc21191da7127f739c558bd12b1ac03129a7afd1
eckart <eckart.inv3@gmail.com>
2016-05-30 17:03:22 -0500

    avance de app
    estructura basica para demo

---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,139 @@
+var app = angular.module('starter.controllers', [])
+
+app.controller('DashCtrl', function($scope,Articulos) {
+    $scope.articulos = Articulos.all();
+});
+
+app.controller('ChatsCtrl', function($scope, Chats ,$http,$sce) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  var defaultHTTPHeaders = {
+      'Content-Type' :  'application/json',
+      'Accept' : 'application/json'
+  };
+    
+  $http.defaults.headers.post = defaultHTTPHeaders;
+    
+    $scope.nota =  {id: new Date().getTime().toString(), mensaje:''};
+    console.log($scope.nota.descripcion);
+  
+    $scope.enviar =  function(){
+      
+//    var urlCompleta = 'http://www.birdev.mx/message_app/public/messages';
+//    var postUrl = $sce.trustAsResourceUrl(urlCompleta);
+//    $http.post(postUrl,$scope.nota).then(function(){
+//        alert('Guardado');
+//    },function(){
+//        alert('error');
+//    });  
+      
+      console.log($scope.nota.id);
+      console.log($scope.nota.mensaje);
+       var link = 'http://www.birdev.mx/message_app/public/messages';
+
+        $http.post(link, {mensaje : $scope.nota.mensaje,identificador: $scope.nota.id}).then(function (res){
+            $scope.response = res.data;
+        });
+      
+      
+  };
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+
+    
+    
+});
+
+app.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+});
+
+app.controller('DiaCtrl', function($scope,$state,$stateParams, $location,   $ionicPopover) {
+    $scope.fecha = new Date($stateParams.fecha);
+});
+
+app.controller('AccountCtrl', function($scope,$state, $location) {
+    
+        //crea el objeto para el datepiker
+//  $scope.onezoneDatepicker = {
+//    date: date, // MANDATORY                     
+//    mondayFirst: false,                
+//    months: months,                    
+//    daysOfTheWeek: daysOfTheWeek,     
+//    startDate: startDate,             
+//    endDate: endDate,                    
+//    disablePastDays: false,
+//    disableSwipe: false,
+//    disableWeekend: false,
+//    disableDates: disableDates,
+//    disableDaysOfWeek: disableDaysOfWeek,
+//    showDatepicker: false,
+//    showTodayButton: true,
+//    calendarMode: false,
+//    hideCancelButton: false,
+//    hideSetButton: false,
+//    highlights: highlights
+//    callback: function(value){
+//        // your code
+//    }
+//};
+    
+    $scope.onezoneDatepicker = {
+    date: new Date(),  
+    mondayFirst: false,                   
+    disablePastDays: false,
+    disableSwipe: true,
+    disableWeekend: false,
+    showDatepicker: true,
+    showTodayButton: true,
+    calendarMode: true,
+    hideCancelButton: false,
+    hideSetButton: false,
+    highlights:  [
+    {
+        date: new Date(2016, 4, 30),
+        color: '#8FD4D9',
+        textColor: '#fff'
+    },
+    {
+        date: new Date(2016, 1, 18)
+    },
+    {
+        date: new Date(2016, 1, 19)
+    },
+    {
+        date: new Date(2016, 1, 20)
+    }
+],
+
+    callback: function(value){
+        console.log(value);
+        pulsado(value);
+    }
+};
+    
+    function pulsado(fecha)
+    {
+        $scope.onezoneDatepicker.highlights = [{
+            date: fecha,
+            color: '#8FD4D9',
+            textColor: '#fff'
+            }];
+        
+        //  $location.path('/dia',{fecha : fecha}) 
+        $state.go('tab.calendario-detalle',{fecha : fecha}); 
+    }
+
+});
+
+app.controller('articuloCompletoCtrl', function($scope, $stateParams, Articulos) {
+  $scope.articulo = Articulos.get($stateParams.articuloId);
+});
---
+++ www/js/app.js
@@ -0,0 +1,122 @@
+(function(){
+var app =  angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','onezone-datepicker'])
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.articulos', {
+    url: '/articulos',
+    views: {
+      'tab-articulos': {
+        templateUrl: 'templates/tab-articulos.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+  .state('tab.articulo-completo', {
+      url: '/articulos/:articuloId',
+      views: {
+        'tab-articulos': {
+          templateUrl: 'templates/articulo-completo.html',
+          controller: 'articuloCompletoCtrl'
+        }
+      }
+    })
+  
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+//    .state('tab.chat-detail', {
+//      url: '/chats/:chatId',
+//      views: {
+//        'tab-chats': {
+//          templateUrl: 'templates/chat-detail.html',
+//          controller: 'ChatDetailCtrl'
+//        }
+//      }
+//    })
+  
+.state('tab.calendario-detalle', {
+    url: '/dia/:fecha',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/calendario-detalle.html',
+        controller: 'DiaCtrl'
+      }
+    }
+  })
+  
+  .state('tab.sintomas', {
+    url: '/sintomas',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/sintomas.html',
+        controller: 'DiaCtrl'
+      }
+    }
+  })
+  
+  .state('tab.anticonceptivo', {
+    url: '/anticonceptivo',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/anticonceptivo.html',
+        controller: 'DiaCtrl'
+      }
+    }
+  })
+  
+  
+.state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/articulos');
+
+});
+    
+    
+}());
